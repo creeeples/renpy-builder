5c65d1aa152e6e0db91b97e0b02d9ad899bf0e77
Creeeples <137629142+creeeples@users.noreply.github.com>
2023-07-09 16:08:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sdk_name=renpy-$1-sdk
-echo "Downloading the specified SDK (${sdk_name})..."
+echo "Downloading RenPy SDK v(${sdk_name})..."
 wget -q https://www.renpy.org/dl/$1/${sdk_name}.tar.bz2
 clear
 
@@ -10,19 +10,6 @@ echo "Setting up the specified SDK (${sdk_name})..."
 tar -xf ./${sdk_name}.tar.bz2
 rm ./${sdk_name}.tar.bz2
 mv ./${sdk_name} ../renpy
-
-# Note: This will be checked regardless of the version of Ren'Py. Caution is
-# advised.
-if [ -d "$2/old-game" ]; then
-    echo "old-game directory detected."
-    if [ -z "$(ls -A "$2/old-game")" ]; then
-        echo "ERROR: old-game is empty. This will cause incompatibility issues."
-        echo "For more information on how the old-game directory works and why"
-        echo "this directory should not be empty, please refer to the documentation"
-        echo "at: https://www.renpy.org/doc/html/build.html#old-game."
-        exit 1
-    fi
-fi
 
 case $3 in
     pc|mac|linux|market|web|android)
